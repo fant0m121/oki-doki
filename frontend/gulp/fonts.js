f1552b529670a0da94c58ffebd1fee1cc1835f77
Maxim Terekhov <mt@poloniumarts.com>
2017-03-17 02:10:07 +0300

    * frontend: исправлена задача для работы со шрифтами

--- frontend/gulp/fonts.js
+++ frontend/gulp/fonts.js
@@ -8,15 +8,13 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
   var dest = path.join(taskTarget, dirs.fonts.replace(/^_/, ''));
 
   gulp.task('fonts', function () {
-    return gulp.src(path.join(dirs.source, dirs.fonts, entries.font))
-    .pipe(plugins.plumber())
-    .pipe(plugins.rename(function(path) {
-      // Remove 'source' directory as well as prefixed folder underscores
-      // Ex: 'src/_fonts' --> '/fonts'
-      path.dirname = path.dirname.replace(dirs.source, '').replace('_', '');
-    }))
-    .pipe(plugins.changed(dest))
-    .pipe(gulp.dest(dest))
-    .pipe(browserSync.stream());
+    gulp.src(path.join(dirs.source, dirs.fonts, entries.font))
+      .pipe(plugins.plumber())
+      .pipe(plugins.rename(function(path) {
+        path.dirname = path.dirname.replace(dirs.source, '').replace('_', '');
+      }))
+      .pipe(plugins.changed(dest))
+      .pipe(gulp.dest(dest))
+      .pipe(browserSync.stream());
   });
 };
