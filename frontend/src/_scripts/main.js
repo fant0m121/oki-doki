1f0815d402925b62b79d29c0b88b14a92e2992d9
Maxim Terekhov <mt@poloniumarts.com>
2017-03-18 19:11:01 +0300

    + frontend: добавлен блок истории проектов на главной

--- frontend/src/_scripts/main.js
+++ frontend/src/_scripts/main.js
@@ -3,10 +3,28 @@
 
 'use strict';
 
-var $ = require('jquery');
-var Link = require('../_modules/link/link');
+var $ = window.jQuery = require('jquery');
+require('owl.carousel');
+
 
 $(function() {
-  new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
+    var owlProject =  $('.js-project-carousel');
+    owlProject.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        dots: false
+    });
+
+    $('.js-project-carousel-next').on('click', function() {
+        owlProject.trigger('next.owl.carousel');
+    });
+    $('.js-project-carousel-prev').on('click', function() {
+        owlProject.trigger('prev.owl.carousel');
+    });
+    owlProject.on('changed.owl.carousel', function(event) {
+        console.log(event.item.index);
+        $('.js-project-carousel-number').text(event.item.index - 1);
+    })
+    console.log('Welcome to Yeogurt!');
 });
